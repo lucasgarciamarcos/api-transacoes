8ddaab4ca9c1115b530ae362150020f79a8f4514
Lucas Garcia Marcos <lucasgmarcos@gmail.com>
2023-09-09 17:00:00 -0300

    Metodo quinhentos implementado

--- app/Http/Controllers/ContaController.php
+++ app/Http/Controllers/ContaController.php
@@ -70,4 +70,35 @@ class ContaController extends Controller
             'saldo' => $conta->saldo
         ], 200);
     }
+
+    public function quinhentos()
+    {
+        // Em um banco como mySQL daria pra fazer desta forma mas vou utilizar uma forma mais simples por se tratar de sqlite
+        // $menorContaIdNaoExistente = Conta::selectRaw('MIN(t1.conta_id + 1) AS menor_conta_id')
+        //    ->from('contas AS t1')
+        //    ->leftJoin('contas AS t2', 't1.conta_id + 1', '=', 't2.conta_id')
+        //    ->whereNull('t2.conta_id')
+        //    ->first();
+
+        // $menorContaId = $menorContaIdNaoExistente->menor_conta_id ?? 1; // Se não encontrar nenhum valor, assume 1 como o menor valor possível
+
+        $menorContaIdExistente = Conta::min('conta_id');
+        $menorContaId = $menorContaIdExistente + 1;
+
+        while (Conta::where('conta_id', $menorContaId)->exists()) {
+            $menorContaId++;
+        }
+
+        $conta = new Conta([
+            'conta_id' => $menorContaId,
+            'saldo' => 500.00
+        ]);
+
+        $conta = Conta::criar($conta);
+
+        return response()->json([
+            'conta_id' => $conta->conta_id,
+            'saldo' => $conta->saldo
+        ], 201);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,8 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->post('/list', 'ContaController@list');
-$router->post('/conta', 'ContaController@create');
-$router->get('/conta', 'ContaController@get');
-$router->get('/transacao', 'TransacaoController@operate');
+$router->post('/list', 'ContaController@list');             // Lista que eu criei para orientação e visualização dos dados
+$router->post('/conta', 'ContaController@create');          // O endpoint "/conta" deve criar e fornecer informações sobre o número da conta e o saldo
+$router->get('/conta', 'ContaController@get');              // "
+$router->post('/quinhentos', 'ContaController@quinhentos'); // Crie um endpoint na API que permita a criação de uma nova conta com um saldo inicial de R$500.
+$router->post('/transacao', 'TransacaoController@create');  // O endpoint "/transacao" será responsável por realizar diversas operações financeiras.
